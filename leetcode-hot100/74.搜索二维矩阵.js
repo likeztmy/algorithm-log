35f4c98823dac69c9a0fb1f94eb7f3df51f7f38f
likeztmy <413713942@qq.com>
2024-05-16 16:02:37 +0800

    feat: 5.16 搜索二维矩阵

---
+++ leetcode-hot100/74.搜索二维矩阵.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=javascript
+ *
+ * [74] 搜索二维矩阵
+ *
+ * https://leetcode.cn/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (49.65%)
+ * Likes:    926
+ * Dislikes: 0
+ * Total Accepted:    405.8K
+ * Total Submissions: 817.3K
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,60]]\n3'
+ *
+ * 给你一个满足下述两条属性的 m x n 整数矩阵：
+ * 
+ * 
+ * 每行中的整数从左到右按非严格递增顺序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 
+ * 
+ * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -10^4 <= matrix[i][j], target <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let left = 0;
+    let right = (matrix.length - 1) * matrix[0].length + matrix[0].length - 1;
+    const jlen = matrix[0].length;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const i = Math.floor(mid / jlen);
+        const j = mid % jlen;
+        if(matrix[i][j] === target) {
+            return true;
+        } else if(matrix[i][j] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+};
+// @lc code=end
+
