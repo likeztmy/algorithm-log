bac78686b573669854319d367d4fe1b2540e99bf
likeztmy <413713942@qq.com>
2024-05-27 15:17:55 +0800

    feat: 5.27 验证二叉搜索树

---
+++ leetcode-hot100/98.验证二叉搜索树.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode.cn/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (37.93%)
+ * Likes:    2344
+ * Dislikes: 0
+ * Total Accepted:    918K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目范围在[1, 10^4] 内
+ * -2^31 <= Node.val <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  let pre;
+  let queue = [];
+  while (root || queue.length) {
+    while (root) {
+      queue.push(root);
+      root = root.left;
+    }
+    root = queue.pop();
+    if (pre !== undefined && pre >= root.val) {
+      return false;
+    } else {
+      pre = root.val;
+    }
+    root = root.right;
+  }
+  return true;
+};
+// @lc code=end
