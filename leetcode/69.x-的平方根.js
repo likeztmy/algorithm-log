08c1fbe5a681030a6e2aaa24d56773f55935ecd4
likeztmy <413713942@qq.com>
2024-07-06 17:24:50 +0800

    feat: 7.6 x的平方根

---
+++ leetcode/69.x-的平方根.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=javascript
+ *
+ * [69] x 的平方根
+ *
+ * https://leetcode.cn/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (38.53%)
+ * Likes:    1555
+ * Dislikes: 0
+ * Total Accepted:    921.4K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '4'
+ *
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+ *
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ *
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：x = 4
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= x <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let i = 0;
+  while (i * i <= x) i++;
+  return i - 1;
+};
+// @lc code=end
