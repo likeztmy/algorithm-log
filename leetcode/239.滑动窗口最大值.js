3ed9a6920993958658eb0efdf110be0c1280628e
likeztmy <413713942@qq.com>
2024-07-28 18:20:14 +0800

    feat: 7.28 滑动窗口最大值

---
+++ leetcode/239.滑动窗口最大值.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=javascript
+ *
+ * [239] 滑动窗口最大值
+ *
+ * https://leetcode.cn/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (48.93%)
+ * Likes:    2828
+ * Dislikes: 0
+ * Total Accepted:    647.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回 滑动窗口中的最大值 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+  const queue = [];
+  const memo = new Map();
+  let left = 0;
+  let right = 0;
+  const ans = [];
+  while (right < nums.length) {
+    while (queue.length && nums[right] > nums[queue[queue.length - 1]]) {
+      queue.pop();
+    }
+    queue.push(right);
+
+    while (queue[0] < left) {
+      queue.shift();
+    }
+    right++;
+    if (right - left === k) {
+      ans.push(nums[queue[0]]);
+      left++;
+    }
+  }
+  return ans;
+};
+// @lc code=end
+
+// 7 2 4 1 0
