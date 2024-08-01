a5139364b019df8610cc5864789eef573869a396
likeztmy <413713942@qq.com>
2024-08-01 17:22:50 +0800

    feat: 8.1 岛屿数量

---
+++ leetcode-hot100/200.岛屿数量.js
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=javascript
+ *
+ * [200] 岛屿数量
+ *
+ * https://leetcode.cn/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (60.55%)
+ * Likes:    2544
+ * Dislikes: 0
+ * Total Accepted:    878.7K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：grid = [
+ * ⁠ ["1","1","1","1","0"],
+ * ⁠ ["1","1","0","1","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：grid = [
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","1","0","0"],
+ * ⁠ ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1
+ * grid[i][j] 的值为 '0' 或 '1'
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+var numIslands = function (grid) {
+  let count = 0;
+
+  const explore = (row, col) => {
+    if (
+      row < 0 ||
+      col < 0 ||
+      row >= grid.length ||
+      col >= grid[0].length ||
+      grid[row][col] === "0"
+    ) {
+      return;
+    }
+    grid[row][col] = "0";
+    explore(row + 1, col);
+    explore(row, col - 1);
+    explore(row - 1, col);
+    explore(row, col + 1);
+  };
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (grid[row][col] == "1") {
+        count++;
+        explore(row, col, grid);
+      }
+    }
+  }
+  return count;
+};
+
+// @lc code=end
